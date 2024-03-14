18e5719b918490476571130613d0e5ea0ce7b7b4
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-03-14 10:57:16 +0100

    test: ensure icons are not zoomed

--- docs/src/content/docs/tests/zoom.md
+++
@@ -1,7 +0,0 @@
----
-title: Zoom tests
----
-
-An SVG image:
-
-![Astro logo](../../../assets/tests/astro-light.svg)
--- packages/starlight-medium-zoom/tests/zoom.test.ts
+++ packages/starlight-medium-zoom/tests/zoom.test.ts
@@ -6,6 +6,15 @@ test('zooms an SVG image', async ({ testPage }) => {
   await expect(testPage.getNthImage(0)).toBeZoomedAfterClick()
 })
 
+test('does not zoom an SVG image from the `<Icon>` component', async ({ testPage }) => {
+  await testPage.goto('zoom')
+
+  const svg = testPage.page.locator('.sl-markdown-content svg:nth-of-type(1)')
+  await svg.click()
+
+  await expect(testPage.getZoomedImage()).not.toBeAttached()
+})
+
 test('closes the zoomed image when using the `Tab` key', async ({ testPage }) => {
   await testPage.goto('zoom')
 
