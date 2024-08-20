d1657fc64c233cd5c56eaeca4a031b25f25bfe9a
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-08-20 16:57:18 +0200

    feat: removes warning when a `MarkdownContent` component override already exists

--- packages/starlight-image-zoom/index.ts
+++ packages/starlight-image-zoom/index.ts
@@ -29,21 +29,14 @@ export default function starlightImageZoomPlugin(userConfig?: StarlightImageZoom
   return {
     name: 'starlight-image-zoom-plugin',
     hooks: {
-      setup({ addIntegration, config, logger, updateConfig }) {
+      setup({ addIntegration, config, updateConfig }) {
         const updatedConfig: Partial<StarlightUserConfig> = { components: { ...config.components } }
 
         if (!updatedConfig.components) {
           updatedConfig.components = {}
         }
 
-        if (config.components?.MarkdownContent) {
-          logger.warn(
-            'It looks like you already have a `MarkdownContent` component override in your Starlight configuration.',
-          )
-          logger.warn(
-            'To use `starlight-image-zoom`, either remove the override or manually render `starlight-image-zoom/components/ImageZoom.astro`.',
-          )
-        } else {
+        if (!config.components?.MarkdownContent) {
           updatedConfig.components.MarkdownContent = 'starlight-image-zoom/overrides/MarkdownContent.astro'
         }
 
